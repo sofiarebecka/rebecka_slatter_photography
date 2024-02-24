7794c25ae0f31f00c28371aa4f413b1bf68433cd
francescoGuglielmi <gug.francesco@gmail.com>
2024-02-24 00:33:26 +0000

    back to traditional Routes

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -7,7 +7,7 @@ import Contacts from '../contacts/Contacts';
 import Headshots from '../Headshots/Headshots';
 import {
   useNavigate,
-  HashRouter,
+  Routes,
   Route,
 } from "react-router-dom";
 
@@ -16,13 +16,13 @@ const App = () => {
     return (
       <>
         <Navbar />
-        <HashRouter>
-          <Route path='/#/' element={<Homepage navigate={useNavigate() }/>}/>
-          <Route path='/#/about' element={<AboutPage navigate={useNavigate() }/>}/>
-          <Route path='/#/work' element={<WorkPage navigate={useNavigate() }/>}/>
-          <Route path='/#/contact' element={<Contacts navigate={useNavigate() }/>}/>
-          <Route path='/#/headshots' element={<Headshots navigate={useNavigate() }/>}/>
-        </HashRouter>
+        <Routes>
+          <Route path='/' element={<Homepage navigate={useNavigate() }/>}/>
+          <Route path='/about' element={<AboutPage navigate={useNavigate() }/>}/>
+          <Route path='/work' element={<WorkPage navigate={useNavigate() }/>}/>
+          <Route path='/contact' element={<Contacts navigate={useNavigate() }/>}/>
+          <Route path='/headshots' element={<Headshots navigate={useNavigate() }/>}/>
+        </Routes>
       </>
     );
 }
