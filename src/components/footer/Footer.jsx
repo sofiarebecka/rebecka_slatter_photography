351e38f600f578f5cec430aab4954bfd8d3dcff8
francescoGuglielmi <gug.francesco@gmail.com>
2025-02-23 13:05:17 +0000

    footed sorted in headshots site

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -3,16 +3,16 @@ import React, { useEffect } from 'react';
 
 const Footer = ({ theme }) => {
 
-  const position = theme === 'light' ? 
-  `${style['pos-relative']} ${style['light']}` :
-  `${style['pos-absolute']} ${style['dark']}`;
+  const profile = theme === 'light' ? 
+  ` ${style['light']}` :
+  ` ${style['dark']}`;
 
   console.log(window.location.pathname);
 
-  const animation = window.location.pathname === '/about' ? ` ${style['animate']}` : '';
+  const animation = window.location.pathname.includes('about') ? ` ${style['animate']}` : '';
 
   return (
-    <footer className={position + animation}>
+    <footer className={profile + animation}>
       <i className="fa-brands fa-instagram fa-2xl"></i>
       <i className="fa-brands fa-linkedin-in fa-2xl"></i>
       <i className="fa-regular fa-envelope fa-2xl"></i>
